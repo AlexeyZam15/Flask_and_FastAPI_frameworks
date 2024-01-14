2bbf560834b05ce01ecf5ade75b934b984df41bd
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2024-01-14 19:51:02 +1000

    added homework_03

---
+++ homework_03/forms.py
@@ -0,0 +1,22 @@
+"""
+Создать форму для регистрации пользователей на сайте.
+Форма должна содержать поля "Имя", "Фамилия", "Email", "Пароль" и кнопку "Зарегистрироваться".
+При отправке формы данные должны сохраняться в базе данных, а пароль должен быть зашифрован.
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
